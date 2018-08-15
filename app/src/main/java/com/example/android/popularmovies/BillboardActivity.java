5fb763ec964fc1283acd87ed71326ca59998fd8e
Paul Gallegos <paul.igb@gmail.com>
2018-08-14 23:28:22 -0700

    Add list_itme_billboard_movie.xml file
    Checks if there is network connection or not.
    If there is network connection, show an image and fetch movies data.
    Otherwise, print a log message.

--- app/src/main/java/com/example/android/popularmovies/BillboardActivity.java
+++ app/src/main/java/com/example/android/popularmovies/BillboardActivity.java
@@ -1,23 +1,39 @@
 package com.example.android.popularmovies;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 
 import com.example.android.popularmovies.Utilities.NetworkUtils;
+import com.squareup.picasso.Picasso;
 
 import java.net.URL;
 
 public class BillboardActivity extends AppCompatActivity {
     private static final String TAG = NetworkUtils.class.getSimpleName();
 
+    public boolean isNetworkAvailable(Context context) {
+        final ConnectivityManager connectivityManager = ((ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE));
+        return connectivityManager.getActiveNetworkInfo() != null && connectivityManager.getActiveNetworkInfo().isConnected();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_billboard);
-        new FetchBillboardInformationTask().execute();
 
+        if(isNetworkAvailable(getApplicationContext())) {
+            ImageView listItemImageView = (ImageView) findViewById(R.id.list_item_image);
+            Picasso.with(this).load("http://i.imgur.com/DvpvklR.png").into(listItemImageView);
+            new FetchBillboardInformationTask().execute();
+        } else {
+            // TODO: Print a message that there is no internet connection or do something else.
+            Log.v(TAG, "No Network connection.");
+        }
     }
 
     public class FetchBillboardInformationTask extends AsyncTask<Void, Void, String> {
